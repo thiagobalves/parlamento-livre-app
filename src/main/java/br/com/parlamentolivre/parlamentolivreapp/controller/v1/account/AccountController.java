38d16d8d84c7fa1a12ec6f9bb08baf0b55ef39eb
Thiago Alves <thiago.balves@hotmail.com>
2023-09-24 13:18:53 -0300

    add account na url base

--- src/main/java/br/com/parlamentolivre/parlamentolivreapp/controller/v1/account/AccountController.java
+++ src/main/java/br/com/parlamentolivre/parlamentolivreapp/controller/v1/account/AccountController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api-parlamento-livre/v1/")
+@RequestMapping("api-parlamento-livre/v1/account/")
 public class AccountController {
 
     AccountService accountService;
